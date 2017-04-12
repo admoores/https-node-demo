150edc5d0447afd79b9cff2216381e8339b661f4
Alex Moores <moores.alexd@gmail.com>
2017-04-11 22:57:03 -0400

    basic node server demo with HTTPS

---
+++ server/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
+const domainName = ''; // This will be whatever domain you used to register your certificates through Let's Encrypt
+
+const key = fs.readFileSync('/etc/letsencrypt/live/' + domainName + '/privkey.pem');
+const cert = fs.readFileSync('/etc/letsencrypt/live/' + domainName + '/fullchain.pem');
+
+const app = express();
+
+const server = https.createServer({
+  key: key,
+  cert: cert
+}, app);
+
+const port = 443;
+
+server.listen(port, () => {
+  console.log('Listening securely on port ' + port);
+});
